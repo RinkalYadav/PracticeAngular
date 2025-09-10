a029ae26b86c405d2ecf45a853926a2f53b39f41
RinkalYadav <rinkalbyadav@gmail.com>
2025-09-10 10:24:15 +0530

    First commit - Angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'youtube_angular_practice';
+  village="Barmohani"
+  hello(){
+    alert("function is working");
+    this.hii();
+    var name:any ="RInkal Yadavv";
+
+    
+  }
+  hii(){
+      alert("hii function is working");
+    }
+    sum(a:number,b:any){
+      alert(a+"ram");
+    }
+    
+  
 }
